b3dedf99b5c83ff01efad9bad6993d7c1e1ad63d
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-01-09 16:50:57 +0700

    Update date class

---
+++ Class/Date/Date.h
@@ -0,0 +1,23 @@
+#ifndef DATE_H
+#define DATE_H
+#include <iostream>
+#include <sstream>
+#define SIZE 50
+using std::string;
+const int daysOfMonth[SIZE] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+class Date {
+    private:
+        int date;
+        int month;
+        int year;
+    public:
+        Date();
+        Date(int date, int month, int year);
+        int countLeapYear();
+        int countDate();
+
+        bool operator < (Date &date2);
+        int operator - (Date &date2);
+        string convertDatetoString();
+};
+#endif
